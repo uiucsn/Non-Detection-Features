b27c8564de82fb8b3929e2f8960c8a88beb6a7d1
Ved Shah <vedshah.email@gmail.com>
2022-03-10 17:44:04 -0600

    Improved feature extraction code to directly produce data frames with passband data, delta t

--- NDFeatures.py
+++ NDFeatures.py
@@ -1,3 +1,4 @@
+from turtle import pos
 import numpy as np
 from numpy.core.defchararray import encode
 import pandas as pd
@@ -7,104 +8,129 @@ import lightkurve as lk
 
 class NDFeatureExtractor:
 
+    # Data for internal use.
+    passbandColors = {
+        'LSST' : {'u ': 'tab:blue', 'g ': 'tab:orange', 'r ': 'tab:green', 'i ': 'tab:red', 'z ': 'tab:purple', 'Y ': 'tab:pink'},
+    }
+
     def __init__(self, dataFrame, survey):
 
         self.dataFrame = dataFrame.to_pandas()
         self.dataFrame['BAND'] = np.array(self.dataFrame['BAND'], dtype=np.str)
         self.survey = survey
-        
+            
     def extractDetectionData(self):
+        """
+        Returns a pandas dataframe with the detection, pre-dection and post-detection 
+        passbands for all every detection in the FITS file along with the delta time 
+        to the next detection (if any) and previous detection (if any). Maintains all
+        the columns from the original FITS file but removes rows for non detections.
 
-        detectionDataFrame = pd.DataFrame(columns = ['BAND', 'FLUXCAL', 'FLUXCALERR'])
+        Returns:
+            Pandas data frame: Consists of the orignal data, passband data and delta
+            times for all the detections in the FITS file.
+        """
 
         # Indexes for the detections
         boolArray = self.dataFrame['PHOTFLAG'] != 0
         idx = np.where(boolArray)[0]
 
         detectionDataFrame  = self.dataFrame.loc[idx, ]
-        
-        self.features['Detection Data'] = detectionDataFrame
+
+        # Extracting features
+        pre_det_pb, post_det_pb = self.getPrePostDetPB(idx)
+        timeToPrev, timeToNext = self.getTimeBetweenDetections(idx)
+
+        # Adding PB data to the DF
+        detectionDataFrame['PRE-BAND'] = pre_det_pb
+        detectionDataFrame['POST-BAND'] = post_det_pb
+
+        # Adding Delta T data to the DF
+        detectionDataFrame['TIME-TO-PREV'] = timeToPrev
+        detectionDataFrame['TIME-TO-NEXT'] = timeToNext
+
         return detectionDataFrame
+
+    def getPrePostDetPB(self, idx):
+        """
+        Returns two lists containing the pre - detection and post - detection passbands 
+        for every detection in the FITS file. 
+
+        Args:
+            idx (numpy array): Indices of the detections in the FITS file.
+
+        Returns:
+            List : pre_det_pb - list containing the pre - detection passbands for all 
+            detections. The entry is an empty string if there is no non detection before 
+            the detection.
+            List : post_det_pb - list containing the post - detection passbands for all 
+            detections. The entry is an empty string if there is no non detection after 
+            the detection.
+        """
+
+        # Predetection observations.
+        pre_det_pb = []
+
+        # Postdetection observations.
+        post_det_pb = []
+
+        # Getting pre and post detection passbands for every detection
+        for i in range(len(idx)):
+
+            pre_index = idx[i] - 1
+            post_index = idx[i] + 1
+
+            # Adding pre detection passband if it exists
+            if pre_index < 0:
+                pre_det_pb.append(None)
+            else:
+                pre_det_pb.append(self.dataFrame['BAND'][pre_index])
+
+            # Adding post detection passband if it exists
+            if post_index >= len(self.dataFrame):
+                post_det_pb.append(None)
+            else:
+                post_det_pb.append(self.dataFrame['BAND'][post_index])
+        
+        return pre_det_pb, post_det_pb
     
-    def extractTimeBetweenSucessiveDetections(self):
-        
-        timeDeltaDataFrame = pd.DataFrame(columns = ['Time Delta'])
+    def getTimeBetweenDetections(self, idx):
+        """
+        Returns two lists containing the time to previous detection and time to next
+        detection for every detection in the FITS file.         
 
-        # Indexes for the detections.
-        boolArray = self.dataFrame['PHOTFLAG'] != 0
-        idx = np.where(boolArray)[0]
+        Args:
+            idx (numpy array): Indices of the detections in the FITS file.
 
-        # Only calculates time delta if there are more than one detections.
-        if len(idx) > 1:
-            for i in idx[1:]:
+        Returns:
+            List: timeToPrev - Contains the time to previous detection. Value is
+            None if there is no previous detection.
+            List: timeToNext - Contains the time to next detection. Value is None
+            if there is no previous detection.
+        """
 
-                deltaTime = self.dataFrame['MJD'][i] - self.dataFrame['MJD'][i - 1]
-                timeDeltaDataFrame = timeDeltaDataFrame.append({'Time Delta': deltaTime}, ignore_index=True)
-        
-        self.features['Time Delta'] = timeDeltaDataFrame
-        return timeDeltaDataFrame
+        timeToNext = []
+        timeToPrev = []
 
-    def extractPrecceedingObservations(self, number = 1):
-        
-        # An array to hold all of the precceeding observations
-        arrayOfDataFrames = []
+        for i in range(len(idx)):
+            
+            # Time to next detection
+            if i == len(idx) - 1:
+                # if it is the last detection, ignore next
+                timeToNext.append('')
+            else:
+                delta_time = self.dataFrame['MJD'][i + 1] - self.dataFrame['MJD'][i]
+                timeToNext.append(delta_time)
 
-        # Indexes for the detection
-        boolArray = self.dataFrame['PHOTFLAG'] != 0
-        idx = np.where(boolArray)[0]
+            # Time to previous detection
+            if i == 0:
+                # If it first detection, ignore previous
+                timeToPrev.append('')
+            else:
+                delta_time = self.dataFrame['MJD'][i] - self.dataFrame['MJD'][i - 1]
+                timeToPrev.append(delta_time)
 
-        for i in idx:
-
-            startIdx = i - number
-
-            # Ensuring that the start index is greater than or equal to 0
-            while startIdx < 0:
-                startIdx += 1
-
-            # A dataframe to store the suceeding observations of a given.
-            preceedingObservationsDataFrame  = self.dataFrame.loc[range(startIdx, i),]
-            arrayOfDataFrames.append(preceedingObservationsDataFrame)
-        
-        self.features['Precceeding Observations'] = arrayOfDataFrames
-        return arrayOfDataFrames
-    
-    def extractSucceedingObservations(self, number = 1):
-        
-        # An array to hold all of the suceeding observations
-        arrayOfDataFrames = []
-
-        # Indexes for the detection
-        boolArray = self.dataFrame['PHOTFLAG'] != 0
-        idx = np.where(boolArray)[0]
-
-        for i in idx:
-
-            endIdx = i + number
-
-            # Ensuring that the end index is less than len 
-            while endIdx >= len(self.dataFrame):
-                endIdx -= 1
-
-            # A dataframe to store the suceeding observations of a given. 
-            succeedingObservationsDataFrame  = self.dataFrame.loc[range(i + 1, endIdx + 1),]
-            arrayOfDataFrames.append(succeedingObservationsDataFrame)
-        
-        self.features['Succeeding Observations'] = arrayOfDataFrames
-        return arrayOfDataFrames
-
-    def extractSignalToNoiseRatio(self, number = 1):
-        
-        # An array to hold all of the n/s ratios
-        signalToNoiseRatio = self.dataFrame['FLUXCAL'] / self.dataFrame['FLUXCALERR']
-        
-        passbands = np.array(self.dataFrame['BAND'], dtype=np.str)
-
-        # A dataframe to store alll the signal to noise ratios along with the passbands
-        signalToNoiseRatioDF = pd.DataFrame(columns = ['Signal to noise'])
-        signalToNoiseRatioDF = signalToNoiseRatioDF.append({'Signal to noise': signalToNoiseRatio, 'BAND': passbands}, ignore_index=True)
-
-        self.features['Signal to noise ratio'] = signalToNoiseRatioDF
-        return signalToNoiseRatioDF
+        return timeToPrev, timeToNext
     
     def buildPseudoLightCurves(self, SNThreshold = 3):
 
@@ -125,9 +151,6 @@ class NDFeatureExtractor:
 
 
         return self.pseudoPassBandLightCurves
-
-    def getFeatures(self):
-        return self.features
 
     def plotInstance(self):
 
@@ -170,8 +193,3 @@ class NDFeatureExtractor:
 
     # Storage for the pseudo light curves
     pseudoPassBandLightCurves = {}
-
-    # Data for internal use.
-    passbandColors = {
-        'LSST' : {'u ': 'tab:blue', 'g ': 'tab:orange', 'r ': 'tab:green', 'i ': 'tab:red', 'z ': 'tab:purple', 'Y ': 'tab:pink'},
-    }
